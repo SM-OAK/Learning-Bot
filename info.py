dd8f1ab8150bce4bd1272ba27b339ce55844739c
SM-OAK <149765588+SM-OAK@users.noreply.github.com>
2025-12-11 11:34:11 +0530

    Update API_ID and API_HASH values

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
+API_ID = int(environ.get('API_ID', '22321078'))
+API_HASH = environ.get('API_HASH', '9960806d290cf4170e43355fcc3687ac')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
