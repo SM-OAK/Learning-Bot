f2f981b8394b022bfdca97befffbf4fdb5049a1c
SM-OAK <149765588+SM-OAK@users.noreply.github.com>
2025-12-27 21:54:49 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -43,7 +43,7 @@ async def save_file(media):
             file_name=file_name,
             file_size=media.file_size,
             mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
+            caption=media.caption if media.caption else None,
             file_type=media.mime_type.split('/')[0]
         )
     except ValidationError:
